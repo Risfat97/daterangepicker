4b4e6b82ac5fd52648b8df2ae2a7ee5949baf280
Sir Taf <sirtaf@tafsir-ndiour.local>
2022-09-16 02:18:04 +0200

    Adding the navigation between month

--- js/daterangepicker.js
+++ js/daterangepicker.js
@@ -1,4 +1,6 @@
 class DateRangePicker {
+    static currentDate = new Date();
+    static month = ['JANV.', 'FÉVR.', 'MARS', 'AVR.', 'MAI', 'JUIN', 'JUIL.', 'AOÛT', 'SEPT.', 'OCT.', 'NOV.', 'DÉC.'];
     static _handleClickTarget(target, calendarDOM) {
         const posTarget = target.getBoundingClientRect();
         calendarDOM.setAttribute('class', 'cal-container');
@@ -6,8 +8,58 @@ class DateRangePicker {
         calendarDOM.style.top = `${posTarget.y + posTarget.height}px`;
     }
 
+    static _handleClickBtnNav(date, calendar, direction) {
+        const calMonth = calendar.querySelector('.cal-month');
+        const calYear = calendar.querySelector('.cal-year');
+        if(direction == 'prev'){
+            date.setMonth(date.getMonth() - 1);
+        } else if(direction == 'next') {
+            date.setMonth(date.getMonth() + 1);
+        }
+        calMonth.textContent = DateRangePicker.month[date.getMonth()];
+        calMonth.dataset.month = date.getMonth();
+
+        calYear.textContent = date.getFullYear();
+        calYear.dataset.month = date.getFullYear();
+        DateRangePicker._generateBodyTable(calendar.querySelector('.cal-body-table'), date.getFullYear(), date.getMonth());
+    }
+
+    static _generateBodyTable(tbody, year, month) {
+        tbody.innerHTML = '';
+        const nbDayThisMonth = (new Date(year, month+1, 0)).getDate();
+        let firstDayMonth = (new Date(year, month, 1)).getDay() - 1;
+        firstDayMonth = (firstDayMonth < 0) ? 7 + firstDayMonth : firstDayMonth;
+        let aux = 0;
+        const daysBeforeFirstDayMonth = [];
+        while(aux !== firstDayMonth) {
+            const td = document.createElement('td');
+            daysBeforeFirstDayMonth.push(td);
+            aux++;
+        }
+        let tr = document.createElement('tr');
+        tr.setAttribute('class', 'cal-row-table');
+        daysBeforeFirstDayMonth.forEach(td => tr.appendChild(td));
+        tbody.appendChild(tr);
+
+        for(let i = 0; i < nbDayThisMonth; i++) {
+            if((aux+i) % 7 === 0){
+                tr = document.createElement('tr');
+                tr.setAttribute('class', 'cal-row-table');
+                tbody.appendChild(tr);
+            }
+            const td = document.createElement('td');
+            if(i+1 === DateRangePicker.currentDate.getDate() && 
+                    DateRangePicker.currentDate.getMonth() === month && 
+                    DateRangePicker.currentDate.getFullYear() === year)
+                td.setAttribute('class', 'cal-current-date');
+            td.textContent = `${i+1}`;
+            tr.appendChild(td);
+        }
+    }
+
     constructor(selector) {
         this.target = document.querySelector(selector);
+        this.date = new Date(DateRangePicker.currentDate.getFullYear(), DateRangePicker.currentDate.getMonth(), 1);
         this.calendar = this._init();
         this.target?.addEventListener('click', (e) => {
             DateRangePicker._handleClickTarget(this.target, this.calendar);
@@ -31,21 +83,30 @@ class DateRangePicker {
         calNavContainer.setAttribute('class', 'cal-nav-container');
         btnNavPrev.setAttribute('class', 'cal-nav nav-prev');
         btnNavNext.setAttribute('class', 'cal-nav nav-next');
+        tableElem.setAttribute('class', 'cal-table');
+        thead.setAttribute('class', 'cal-head-table');
+        tbody.setAttribute('class', 'cal-body-table');
 
         calSelectContainer.innerHTML = `
-            <span class="month">SEPT.</span>
-            <span class="year">2022</span>
+            <span class="cal-month" data-month="${this.date.getMonth()}">${DateRangePicker.month[this.date.getMonth()]}</span>
+            <span class="cal-year" data-year="${this.date.getFullYear()}">${this.date.getFullYear()}</span>
             <hr>
         `;
         btnNavPrev.textContent = '‹';
+        btnNavPrev.onclick = (e) => {
+            DateRangePicker._handleClickBtnNav(this.date, this.calendar, 'prev');
+        };
         btnNavNext.textContent = '›';
+        btnNavNext.onclick = (e) => {
+            DateRangePicker._handleClickBtnNav(this.date, this.calendar, 'next');
+        };
         calNavContainer.appendChild(btnNavPrev);
         calNavContainer.appendChild(btnNavNext);
         calHeader.appendChild(calSelectContainer);
         calHeader.appendChild(calNavContainer);
 
         thead.innerHTML = `
-            <tr>
+            <tr class="cal-row-table">
                 <th>L</th>
                 <th>M</th>
                 <th>M</th>
@@ -55,16 +116,8 @@ class DateRangePicker {
                 <th>D</th>
             </tr>
         `;
-        let tr;
-        for(let i = 0; i < 30; i++) {
-            if(i % 7 === 0){
-                tr = document.createElement('tr');
-                tbody.appendChild(tr);
-            }
-            const td = document.createElement('td');
-            td.textContent = `${i+1}`;
-            tr.appendChild(td);
-        }
+
+        DateRangePicker._generateBodyTable(tbody, this.date.getFullYear(), this.date.getMonth());
         tableElem.appendChild(thead);
         tableElem.appendChild(tbody);
 
